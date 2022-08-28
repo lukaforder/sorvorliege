e543963162c9dabcc83231b303cb7a4f143ac34e
Alan Panayotov <alanpanayotov@gmail.com>
2022-08-28 21:39:23 +0100

    fix: improve server name editor

--- app/src/lib/components/ServerView.svelte
+++ app/src/lib/components/ServerView.svelte
@@ -6,9 +6,17 @@
   import { getContext } from "svelte";
   export let server: ServerInfo | null = null;
 
+  let editing = false;
+  let name = "Select a server.";
+
+  $: if (!editing) {
+    name = server?.name ?? "Select a server.";
+  }
+
   const ws: WSStore<State> = getContext("ws");
 
   const change_name = (name: string, el: HTMLInputElement) => {
+    editing = false;
     console.log("attmept to set name to", name)
     if(name === "") el.value = server?.name ?? "Select a server."; 
     if(!name || !server) return;
@@ -20,26 +28,36 @@
         name,
       }
     })
+    server.name = name;
+    el.innerText = name;
   };
   const on_name_changed = (e: Event) => {
     const target = e.target as HTMLInputElement;
-    change_name(target.value, target);
+    change_name(target.innerText, target);
   }
   const on_name_key = (e: KeyboardEvent) => {
     if (e.key !== "Enter" || !e.currentTarget) return;
+    e.preventDefault();
     const target = e.currentTarget as HTMLInputElement;
-    change_name(target.value, target);
+    target.blur();
+    change_name(target.innerText, target);
   }
 </script>
 
-<article>
-  <header class:noserver="{server === null}">
-    <input value={
-        (server?.name ?? "Select a server.")
-      }
-      on:change="{on_name_changed}"
-      on:keypress="{on_name_key}"
-      disabled="{!server}"/>
+<article class:noserver="{server === null}">
+  <header>
+    {#if server}
+      <h1
+        on:focus="{() => editing = true}"
+        on:blur="{on_name_changed}"
+        on:keypress="{on_name_key}"
+        disabled="{!server}"
+        contenteditable="true"
+        bind:textContent="{name}">
+      </h1>  
+    {:else}
+      <h1>Select a server.</h1>
+    {/if}
     <button>Edit</button>
     <button>Power</button>
     <button>Open</button>
@@ -47,12 +65,24 @@
     <button>Delete</button>
   </header>
   <section>
-    <label for="comm_type">Communicator Type</label>
-    <select name="comm_type">
-      {#each Object.keys(CommunicatorType) as c}
-        <option value="{c}">{c}</option>
-      {/each}
-    </select>
+    <ul>
+      <li>
+        <label for="comm_type">Communicator Type</label>
+        <select name="comm_type" on:change="{()=>{}}">
+          {#each Object.keys(CommunicatorType) as c}
+            <option value="{c}">{c}</option>
+          {/each}
+        </select>
+      </li>
+      <li>
+        <label for="comm_port">Port</label>
+        <input type="text" name="comm_port" on:change="{()=>{}}"/>
+      </li>
+      <li>
+        <label for="comm_ip">IP</label>
+        <input type="text" name="comm_ip" on:change="{()=>{}}"/>
+      </li>
+    </ul>
   </section>
 </article>
 
@@ -72,7 +102,7 @@
     &:enabled {
       cursor: pointer;
     }
-    &:enabled:hover {
+    &:enabled:hover, &[contenteditable]:hover {
       background: rgba(0,0,0,0.2);
     }
 
@@ -110,14 +140,19 @@
   header {
     display: flex;
     margin: 1em 1em 1em 1em;
-    align-items: baseline;
-    &>input {
+    align-items: flex-end;
+    &>h1 {
       @include minimal-input;
       margin: 0 .5em 0 0;
+      padding: 0.2em 0.3em;
 
-      width: min-content;
+      min-width: 2em;
+      max-width: 10em;
+      width: max-content;
 
-      height: 2em;
+      // word-break: break-all;
+      word-wrap: break-word;
+      overflow: hidden;
 
       font-size: 1.5em;
 
@@ -130,10 +165,45 @@
       padding: 0.5em 0.8em;
     }
     border-bottom: 1px solid #2E3338;
+  }
 
-    &.noserver {
-      button {
-        visibility: hidden;
+  .noserver {
+    button, section {
+      visibility: hidden;
+    }
+  }
+
+  section {
+    margin: 2em 2em 1em 2em;
+
+    ul {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+
+      gap: 0.25em 0.5em;
+      
+      margin: 0;
+      padding: 0;
+
+      list-style: none;
+      color: #888A8B;
+      li {
+        display: contents;
+      }
+
+      label {
+        text-align: right;
+      }
+
+      select, input {
+        @include minimal-input;
+        height: 1.25em;
+        font-size: 1rem;
+      }
+
+      select {
+        padding-left: 0;
       }
     }
   }
